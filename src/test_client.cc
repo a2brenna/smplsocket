194bb4860ecc420b112c85ab7e2d5abd4aa3bcae
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-09-30 15:48:38 -0400

    Default to testing large messages

--- src/test_client.cc
+++ src/test_client.cc
@@ -6,19 +6,17 @@
 #include <string>
 
 int main(int argc, char *argv[]){
-    std::cout << "Starting test_server..." << std::endl;
-
     std::unique_ptr<smpl::Remote_Address> uds(new smpl::Remote_UDS("/tmp/smplsocket_test.sock"));
     std::unique_ptr<smpl::Channel> channel(uds->connect());
 
-    const std::string message = "Test";
+    const std::string message(1000000000, 'a');
     const auto start = std::chrono::high_resolution_clock::now();
     channel->send(message);
     const std::string received_message = channel->recv();
     const auto end = std::chrono::high_resolution_clock::now();
     const auto elapsed_time = end - start;
 
-    std::cout << elapsed_time.count() << std::endl;
+    std::cout << received_message.size() << " " << elapsed_time.count() << std::endl;
 
     return 0;
 }
